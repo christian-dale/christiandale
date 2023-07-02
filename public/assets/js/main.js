db4e2a3dab81f2f4960fbf8123177a2e01e1c162
christian-dale <jchaukikv@gmail.com>
2023-07-02 13:10:28 +0200

    Fix bug with cookie policy displaying for a brief second even tough accepted

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,11 @@
-if (localStorage.getItem("cookiePolicy")) {
-    document.querySelector(".cookie-policy").remove();
+if (localStorage.getItem("cookiePolicy") == null) {
+    document.querySelector(".cookie-policy").style.display = "block";
 }
 
 if (document.querySelector(".cookie-policy") != null) {
     document.querySelector(".btn-cookie-policy-accept").addEventListener("click", function() {
         localStorage.setItem("cookiePolicy", true);
+
         document.querySelector(".cookie-policy").remove();
     });
 }
