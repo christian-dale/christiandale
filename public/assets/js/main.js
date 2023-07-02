bc67d8aa93745a73077a5f9e5d7db0caa9b917c1
christian-dale <jchaukikv@gmail.com>
2023-07-02 18:18:15 +0200

    Improvements to script loading times

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,13 +1,15 @@
-if (localStorage.getItem("cookiePolicy") == null) {
-    document.querySelector(".cookie-policy").style.display = "block";
-}
-
-if (document.querySelector(".cookie-policy") != null) {
-    document.querySelector(".btn-cookie-policy-accept").addEventListener("click", function() {
-        localStorage.setItem("cookiePolicy", true);
-
-        document.querySelector(".cookie-policy").remove();
-    });
-}
-
-hljs.highlightAll();
+document.addEventListener("DOMContentLoaded", function() {
+    if (localStorage.getItem("cookiePolicy") == null) {
+        document.querySelector(".cookie-policy").style.display = "block";
+    }
+    
+    if (document.querySelector(".cookie-policy") != null) {
+        document.querySelector(".btn-cookie-policy-accept").addEventListener("click", function() {
+            localStorage.setItem("cookiePolicy", true);
+    
+            document.querySelector(".cookie-policy").remove();
+        });
+    }
+    
+    hljs.highlightAll();
+});
